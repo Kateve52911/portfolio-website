16814052f90d9ae3df3356460748fa4742c28efd
Kateve52911 <kateve52911@stud.noroff.no>
2025-03-02 17:24:04 +0100

    updated header and footer, included indicators int the header, added an about me section

---
+++ js/utils/updateYear.js
@@ -0,0 +1,5 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Set the current year in the span with id "current-year"
+document.getElementById("current-year").textContent = currentYear;
